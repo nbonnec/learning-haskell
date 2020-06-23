40d216058bdc2937c34d30622bc93444a4f32045
nico <the.vegeta@gmail.com>
2020-06-23 08:49:44 +0200

    Improve skills with codechef.

---
+++ codechef/HS08TEST/main.hs
@@ -0,0 +1,25 @@
+import Data.Text
+import Numeric
+
+withdraw :: Int -> Float -> Float
+withdraw amount balance
+    | amount_f > balance = balance
+    | (balance - amount_f - 0.5 < 0) = balance
+    | (mod amount 5 /= 0) = balance
+    | otherwise = balance - amount_f - 0.5
+    where amount_f = fromIntegral amount
+
+formatFloatN floatNum numOfDecimals = showFFloat (Just numOfDecimals) floatNum ""
+
+main = do
+
+    input <- getLine
+
+    let input_splitted = splitOn (pack " ") (pack input)
+
+    let amount = read (unpack (input_splitted !! 0)) :: Int
+    let balance = read (unpack (input_splitted !! 1)) :: Float
+
+    let new_balance = withdraw amount balance
+
+    putStrLn(show new_balance)
